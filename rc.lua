e41c138bed92699a9c638d0f70e56313b2fdcdc3
Vadim Cherepanov <xenos26@gmail.com>
2024-12-13 22:23:42 +0700

    fix: fix shortcut for toggle on top client

--- rc.lua
+++ rc.lua
@@ -601,7 +601,7 @@ clientkeys = gears.table.join(
   awful.key({ modkey }, "o", function(c)
     c:move_to_screen()
   end, { description = "move to screen", group = "client" }),
-  awful.key({ modkey }, "t", function(c)
+  awful.key({ modkey, "Mod1" }, "t", function(c)
     c.ontop = not c.ontop
   end, { description = "toggle keep on top", group = "client" }),
   awful.key({ modkey }, "n", function(c)
